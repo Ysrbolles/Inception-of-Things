fceaf9ecc801aee11cd73b0aebec67d885e54086
ybolles <ysrbolles@gmail.com>
2022-02-17 18:34:21 +0100

    🐛 fix VagrantFile issues

--- p1/.vagrant/bundler/global.sol
+++ p1/.vagrant/bundler/global.sol
@@ -1,1 +1,1 @@
-{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.30.0"]]],"checksum":"40b2d2826507193788cdfd3711a0825df8ed639b7beeed55bc68dd617118b9b6","vagrant_version":"2.2.19"}
+{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.30.0"]]],"checksum":"b993e0726355c740d4a90599edbeda8008a55eb7105f1bfea9ec504476d92b07","vagrant_version":"2.2.9"}
