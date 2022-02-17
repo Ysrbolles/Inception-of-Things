acacd1cf44a4fd084d34c8b9c6cca57b7a637b67
ybolles <ysrbolles@gmail.com>
2022-02-17 18:33:48 +0100

    🐛 Fix VagrantFile issues

--- p1/Scripts/ybollesS.sh
+++ p1/Scripts/ybollesS.sh
@@ -2,4 +2,3 @@ curl -sfL https://get.k3s.io | sh -
 echo "Sleeping for 5 seconds to wait for k3s to start"
 sudo cp /var/lib/rancher/k3s/server/token /vagrant_shared 
 sudo cp /etc/rancher/k3s/k3s.yaml /vagrant_shared
-		
