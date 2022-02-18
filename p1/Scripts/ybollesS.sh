fc337b068934f6cefe2a7c60006e8d9a15387b98
Yassir BOLLES <ysrbolles@gmail.com>
2022-02-18 16:47:45 +0100

    Fix Master server issue

--- p1/Scripts/ybollesS.sh
+++ p1/Scripts/ybollesS.sh
@@ -1,4 +1,6 @@
+export INSTALL_K3S_EXEC="--bind-address=192.168.42.110 --node-external-ip=192.168.42.110 --flannel-iface=eth1"
 curl -sfL https://get.k3s.io | sh -
 echo "Sleeping for 5 seconds to wait for k3s to start"
-sudo cp /var/lib/rancher/k3s/server/token /vagrant_shared 
-sudo cp /etc/rancher/k3s/k3s.yaml /vagrant_shared
+sleep 5
+cp /var/lib/rancher/k3s/server/token /vagrant_shared 
+cp /etc/rancher/k3s/k3s.yaml /vagrant_shared
