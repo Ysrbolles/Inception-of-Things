79cf98e7de3ae5732649514f45cedee32352aedc
ybolles <ysrbolles@gmail.com>
2022-02-20 19:09:04 +0100

    📝 Create P2 folder

--- p1/.vagrant/bundler/global.sol
+++
@@ -1,1 +0,0 @@
-{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.30.0"]]],"checksum":"b993e0726355c740d4a90599edbeda8008a55eb7105f1bfea9ec504476d92b07","vagrant_version":"2.2.9"}
--- p2/Scripts/ybollesS.sh
+++ p2/Scripts/ybollesS.sh
@@ -1,6 +1,7 @@
 export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --bind-address=192.168.42.110 --node-ip=192.168.42.110 --tls-san $(hostname) --advertise-address=192.168.42.110"
+echo "Installing k3s"
 curl -sfL https://get.k3s.io | sh -
-echo "Sleeping for 5 seconds to wait for k3s to start"
-sleep 5
-cp /var/lib/rancher/k3s/server/token /vagrant_shared 
-cp /etc/rancher/k3s/k3s.yaml /vagrant_shared
+echo "Finished installing k3s"
+
+echo "Deploying app1"
+/usr/local/bin/kubectl apply -f /vagrant_shared/app1/
