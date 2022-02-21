d6c45c48a006647ec3051477af53cdc3b4d4bbe6
ybolles <ysrbolles@gmail.com>
2022-02-21 18:49:22 +0100

    🐛 fix ingress files

--- p2/Scripts/ybollesS.sh
+++ p2/Scripts/ybollesS.sh
@@ -1,12 +1,11 @@
-export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --bind-address=192.168.42.110 --node-ip=192.168.42.110 --tls-san $(hostname) --advertise-address=192.168.42.110"
-echo "Installing k3s"
+export K3S_KUBECONFIG_MODE="644"
+export INSTALL_K3S_EXEC="--flannel-iface eth1"
 curl -sfL https://get.k3s.io | sh -
 echo "Finished installing k3s"
 
 echo "Deploying app1"
-/usr/local/bin/kubectl apply -f /vagrant_shared/app1/
-/usr/local/bin/kubectl apply -f /vagrant_shared/app2/
-/usr/local/bin/kubectl apply -f /vagrant_shared/app3/
-/usr/local/bin/kubectl apply -f /vagrant_shared/services.yaml
+/usr/local/bin/kubectl apply -f /vagrant_shared/app1/deployment.yaml
+/usr/local/bin/kubectl apply -f /vagrant_shared/ingress.yaml
+# /usr/local/bin/kubectl apply -f /vagrant_shared/app3/
 echo "Finished deploying app1"
 
